9030d88d274e68b2e35720cacecaf2b3a80a87a2
Jim <jim.lemmers@gmail.com>
2015-10-10 12:14:22 +0200

    finished the query and made a routine to add objects to which are on the route

--- app/views.py
+++ app/views.py
@@ -5,7 +5,14 @@ from SPARQLWrapper import SPARQLWrapper, RDF, JSON
 import requests
 import math
 import json
-from route import *
+from app import helper
+from app import route_json
+
+prefixes = 'prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' +\
+           'prefix owl: <http://www.w3.org/2002/07/owl#>' +\
+           'prefix xsd: <http://www.w3.org/2001/XMLSchema#>' +\
+           'prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>'
+r = 0.1 / 111
 
 
 @app.route('/', )
@@ -29,22 +36,27 @@ def sparql():
 
     # shortest_route = requests.get(app.config['maps_base_url'],
     #                               params=payload).json()
-    shortest_route = route
+    shortest_route = route_json.route
 
-    pprint.pprint(shortest_route)
     sparql = SPARQLWrapper(app.config['endpoint'])
+
     query = 'select ?place ?lat ?lon where { ?place a scr:Place .' +\
         '?place geo:lat ?lat . ?place geo:long ?lon . }'
 
-    sparql.setQuery(query)
-    sparql.setReturnFormat(RDF)
+    sparql.setQuery(prefixes + query)
+    sparql.setReturnFormat(JSON)
     sparql.addParameter('Accept', 'application/sparql-results+json')
     sparql.addParameter('reasoning', 'true')
 
     response = sparql.query().convert()
+    if response['results']['bindings']:
+        objs = response['results']['bindings']
+
     def in_circle(center_x, center_y, radius, x, y):
-            square_dist = (center_x - x) ** 2 + (center_y - y) ** 2
-            return square_dist <= radius ** 2
+        center_x, center_y, x, y = map(float, (center_x, center_y, x, y))
+        square_dist = (center_x - x) ** 2 + (center_y - y) ** 2
+        return square_dist <= radius ** 2
+
     circles = []
 
     if shortest_route:
@@ -56,9 +68,24 @@ def sparql():
             end_lat, end_lon = end['lat'], end['lng']
 
             distance = step['distance']['value']
-            if distance > 250:
-                num_circles = int(distance / 200)
-    return jsonify(shortest_route)
+            if distance > 150:
+                num_circles = int(distance / 100)
+                print('############################')
+                print(num_circles)
+                for i in range(0, num_circles + 1):
+                    print(i/(num_circles+1))
+                    lat, lon = helper.intermediate_point((start_lat, start_lon), (end_lat, end_lon), i/(num_circles + 1))
+                    circles.append({'lat': lat, 'lon': lon})
+                print('############################')
+
+    on_route = [obj for c in circles for obj in objs if
+                in_circle(c['lat'], c['lon'], r, obj['lat']['value'],
+                          obj['lon']['value'])]
+
+    print(len(on_route))
+    print(len(circles))
+
+    return render_template('map.html', route=steps, circles=circles, )
 
     """
     if endpoint and query :
