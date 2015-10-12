d4a5bd7f14bd79c344035b0966ba46d553672ea8
Jim <jim.lemmers@gmail.com>
2015-10-12 15:36:06 +0200

    added helper and query, next time config will be removed from __init__

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,5 @@ app.config['endpoint'] = 'http://localhost:5820/scenicroute/query'
 app.config['reasoning'] = 'true'
 from app import views  # noqa
 from app import helper  # noqa
-from app import route_json
+from app import query  # noqa
+from app import route_json  # noqa
