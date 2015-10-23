b0d7f01dd45fd29385140f6c15525048cd650976
Jim <jim.lemmers@gmail.com>
2015-10-24 01:57:21 +0200

    commit for report

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@ import re
 import rd2wgs84 as rd_convert
 # import rd2wgs84 as jesus
 
-base_url = 'http://monumentenregister.cultureelerfgoed.nl/php/main.php?cAction=search&cOffset=1&cLimit=%s&oOrder=ASC&cSubmit=1&sProvincie=Noord-Holland&sGemeente=Amsterdam&sPlaats=&sStraat=&sHuisnummer=&sPostcode=&sOmschrijving=&sCompMonNr=&sCompMonName=&sStatus=&sHoofdcategorie=&sSubcategorie=&sFunctie='
-monument_url = 'http://monumentenregister.cultureelerfgoed.nl/php/main.php?cAction=show&cOffset=2&cLimit=25&cOBJnr=%s&oOrder=ASC&cLast=7511&oField=OBJ_RIJKSNUMMER&sCompMonNr=&sCompMonName=&sStatus=&sProvincie=Noord-Holland&sGemeente=Amsterdam&sPlaats=&sStraat=&sHuisnummer=&sPostcode=&sFunctie=&sHoofdcategorie=&sSubcategorie=&sOmschrijving=&ID=0&oField=OBJ_RIJKSNUMMER'
+base_url = 'http://monumentenregister.cultureelerfgoed.nl/php/main.php?cAction=search&cOffset=&cLimit=&oOrder=ASC&cSubmit=1&sProvincie=Noord-Holland&sGemeente=Amsterdam&sPlaats=&sStraat=&sHuisnummer=&sPostcode=&sOmschrijving=&sCompMonNr=&sCompMonName=&sStatus=&sHoofdcategorie=&sSubcategorie=&sFunctie='
+monument_url = 'http://monumentenregister.cultureelerfgoed.nl/php/main.php?cAction=show&cOffset=&cLimit=25&oOrder=ASC&cLast=7511&oField=OBJ_RIJKSNUMMER&sCompMonNr=&sCompMonName=&sStatus=&sProvincie=Noord-Holland&sGemeente=Amsterdam&sPlaats=&sStraat=&sHuisnummer=&sPostcode=&sFunctie=&sHoofdcategorie=&sSubcategorie=&sOmschrijving=&ID=0&oField=OBJ_RIJKSNUMMER&cOBJnr=%s'
 results = []
 
 
@@ -45,25 +45,31 @@ def parse_attr(selector, attr):
     return [sel.attrs.get(attr) for sel in selector]
 
 
-chunks = ['5000', '2511']
-for chunk in chunks:
-    monument_list = bs(req.get(base_url % (chunk)).text)
-    urls = parse_href(monument_list.select('td a')[1:-1])
+def main():
+    monument_list = bs(req.get(base_url).text)
+    urls = parse_href(monument_list.select('td a'))[1:-1]
     urls = [url.split("'")[1] for url in urls]
     for url in urls:
-        print(monument_url % (url))
         monument = bs(req.get(monument_url % (url)).text)
         attrs = monument.select('.tabeltype1 td')
         attrs.pop(0)
         attrs.pop(24)
         attrs.pop(42)
         p_attrs = parse_table(attrs)
-        p_attrs['url'] = monument_url
+        url = monument_url % (url)
+        print(monument_url % (url))
         x, y = p_attrs['X-Y coörd'].split('-')
         if x and y:
             x, y = int(x), int(y)
-            p_attrs['lat'], p_attrs['lon'] = rd_convert.convert(x, y)
-        results.append(p_attrs)
+            lat, lon = rd_convert.convert(x, y)
+            results.append({'url': url,
+                            'naam': p_attrs.get('Monumentnaam', None),
+                            'lat': lat,
+                            'lon': lon,
+                            })
 
-with open('results.json', 'w') as dump:
-    json.dump(results, dump)
+    with open('monumenten_best.json', 'w') as dump:
+        json.dump(results, dump)
+
+if __name__ == '__main__':
+    main()
