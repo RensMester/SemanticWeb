84f0570c2f86608df0d84379d20b37010ac5504b
Jim <jim.lemmers@gmail.com>
2015-10-24 01:59:38 +0200

    final commit of app removed api key

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['maps_API_KEY'] = 'AIzaSyAFxFgbANCIn7i6Wpwve3_DXeOc-teeUvI'
+app.config['maps_API_KEY'] = ''
 app.config['maps_base_url'] = 'https://maps.googleapis.com/maps/api/directions/json?'
-app.config['circle_radius'] = float(0.25 / 111)
 app.config['endpoint'] = 'http://localhost:5820/scenicroute/query'
 app.config['reasoning'] = 'true'
 from app import views  # noqa
